11ad215d77f0344ca85ebf3a2f4fe00ba75be5b9
Olu onasanya <olu.onasanya@britishgas.co.uk>
2019-09-23 15:50:17 +0100

    Initial Tests

---
+++ src/features/find-mortgage-rate.feature
@@ -0,0 +1,8 @@
+Feature: Find a mortgage rate
+  As a new customer
+  I want to find the mortgage rates available
+  So that I can decide whether to switch my mortgage to Nationwide
+
+  @Pending
+  Scenario: Find mortgage rates
+    Given I open the url "https://www.nationwide.co.uk"
---
+++ wdio.conf.js
@@ -0,0 +1,63 @@
+const baseUrl = 'https://nationwide.co.uk';
+
+
+exports.config = {
+
+
+    specs: [
+        './e2e/tests/**/*.js',
+    ],
+
+    exclude: [
+
+    ],
+
+    //
+    maxInstances: 10,
+
+
+    capabilities: [{
+
+        maxInstances: 5,
+        //
+        browserName: 'chrome',
+    }],
+
+
+    sync: true,
+
+    logLevel: 'verbose',
+
+    coloredLogs: true,
+
+    deprecationWarnings: false,
+
+    bail: 0,
+
+    screenshotPath: './errorShots/',
+
+    baseUrl,
+
+    waitforTimeout: 130000,
+
+    connectionRetryTimeout: 912000,
+
+    connectionRetryCount: 3,
+
+    services: ['selenium-standalone'],
+
+    framework: 'jasmine',
+    before() {
+        require('@babel/register');
+    },
+
+    jasmineNodeOpts: {
+
+        defaultTimeoutInterval: 10000,
+
+        expectationResultHandler(passed, assertion) {
+
+        },
+    },
+
+};
